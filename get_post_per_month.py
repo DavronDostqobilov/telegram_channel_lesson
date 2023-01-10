8ed64a379f87bc748e0fab69a942a90cc4f6f15b
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-10 16:08:27 +0500

    get_post_per_month

--- get_post_per_month.py
+++ get_post_per_month.py
@@ -11,4 +11,18 @@ def get_post_per_month(data:dict)->dict:
     Returns: 
         dict: a dictionary with the number of posts for each month
     """
-    return
+    messages=data['messages']
+    k=0
+    m=0
+    n=0
+    for i in messages:
+        str1=i['date']
+        if str1[5:7]=='09':
+            k+=1
+        if str1[5:7]=='10':
+            m+=1
+        if str1[5:7]=='11':
+            n+=1
+    return {'Sentabr': k,'Oktabr':m, 'Noyabr':n}
+x=fromJson('result.json')
+print(get_post_per_month(x))
