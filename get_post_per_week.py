bf42de7a51954d6a8af96c838ae0e64f262fcddc
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-18 00:59:15 +0500

    get_post_per_week

--- get_post_per_week.py
+++ get_post_per_week.py
@@ -1,3 +1,4 @@
+import datetime
 from read_data import fromJson
 
 def get_post_per_week(data:dict,month:int)->dict:
@@ -11,5 +12,27 @@ def get_post_per_week(data:dict,month:int)->dict:
     Returns: 
         dict: a dictionary with the number of posts for each week.
     """
-    return
+    list1=[]
+    month_data={}
+    messages=data['messages']
+    for i in messages:
+        str1=i['date'].split('T')[0]
+        str2=str1.split('-')
+        
+        year=int(str2[0])
+        month=int(str2[1])
+        day=int(str2[2])
+
+        date=datetime.date(year, month, day)
+        week_day=date.weekday()
+
+        list1.append(week_day)
+        month_data.setdefault(week_day, 0)
+        month_data[week_day]+=1
+        
+
+
+    return month_data
+x=fromJson('result.json')
+print(get_post_per_week(x,10))
 
